9fadf2040104586a0487175cd84978ceedf13b53
sbrezdeniuk87 <sbrezdeniuk@gmail.com>
2019-03-14 09:31:20 +0200

    add navigation

--- blackJack/App.js
+++ blackJack/App.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {createAppContainer, createStackNavigator} from 'react-navigation'
+
+import * as firebase from "firebase";
+firebase.initializeApp({apiKey: "AIzaSyCfT_IM5P4Fj1D1RfLxhR860CJi6i3wUdg",
+                        authDomain: "black-jack-5d7d7.firebaseapp.com",
+                        databaseURL: "https://black-jack-5d7d7.firebaseio.com/",
+                        storageBucket: "gs://black-jack-5d7d7.appspot.com"});
 
 import PlayTable from './containers/PlayTable'
+import Begin from './containers/Begin'
+import Login from './containers/Login'
+import Registration from './containers/Registration'
+import Profil from './containers/Profil'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -10,13 +21,28 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const MainNavigator = createStackNavigator(
+    {
+      BEGIN: {screen: Begin},
+      PLAY_TABLE: {screen: PlayTable},
+      LOGIN: {screen: Login},
+      REGISTRATION: {screen: Registration},
+      PROFIL: {screen: Profil}
+    },
+    {
+        headerMode: 'none'
+    }
+)
+
+const AppContainer = createAppContainer(MainNavigator)
+
+//export default App
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <PlayTable />
-      </View>
+        <AppContainer />
     );
   }
 }
